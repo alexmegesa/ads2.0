cbfb31cd8ee684675e8c076c955d3e68d03a8745
alexmgs <alekseyhamow@gmail.com>
2022-02-20 16:38:47 +0700

    Добавил функцию проверки отсортированности чисел в файле

--- lab1/lab1/Source.cpp
+++ lab1/lab1/Source.cpp
@@ -5,6 +5,34 @@
 
 using namespace std;
 
+bool isFileContainsSortedArray(const std::string& fileName)
+{
+	ifstream File(fileName);
+	if (!File.is_open())
+	{
+		cout << "Error: file is not open!" << endl;
+		return 0;
+	}
+
+	int x, y;
+
+	File >> x;
+	while (!File.eof())
+	{
+		File >> y;
+		if (x > y)
+			return 0;
+
+		x = y;
+
+	}
+
+	File.close();
+	return 1;
+
+}
+
+
 int main()
 {
 
