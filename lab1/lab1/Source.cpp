53f806b78f1a46b1e607594c1a0dddb6df09afcd
alexmgs <alekseyhamow@gmail.com>
2022-02-21 20:27:37 +0700

    Написал фазу разбиения

--- lab1/lab1/Source.cpp
+++ lab1/lab1/Source.cpp
@@ -50,8 +50,52 @@ bool createFileWithRandomNumbers(const std::string& fileName, const int numbersC
 
 }
 
+void splitting(const string& fileName1, const string& fileName2, const string& fileName3, int p)
+{
+	ifstream f1(fileName1);
+	ofstream f2(fileName2);
+	ofstream f3(fileName3);
+
+
+	int n = 0, i, x;
+	
+
+	f1 >> x;
+	while (!f1.eof())
+	{
+		i = 0;
+		while (!f1.eof() && i < p)
+		{
+			if (n == 0)
+			{
+				f2 << x << ' ';
+			}
+			else
+			{
+				f3 << x << ' ';
+			}
+
+			f1 >> x;
+			i++;
+		}
+
+		n = 1 - n;
+	}
+}
 
 int main()
-{
+{		
+	int numbersCount, maxNumberValue;
+	cout << "enter count of numbers:	";
+	cin >> numbersCount;
+	cout << "\nenter max number value:	";
+	cin >> maxNumberValue;
+
+
+	createFileWithRandomNumbers("file.txt", numbersCount, maxNumberValue);
+	
+	splitting("file.txt", "file1.txt", "file2.txt", 2);
+
+	
 
 }
