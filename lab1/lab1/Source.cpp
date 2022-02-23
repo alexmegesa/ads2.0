469231caa99a3b2c6bd00425cd65a1bd32e5f7a7
alexmgs <alekseyhamow@gmail.com>
2022-02-23 17:27:39 +0700

    Написал фазу слияния

--- lab1/lab1/Source.cpp
+++ lab1/lab1/Source.cpp
@@ -81,21 +81,118 @@ void splitting(const string& fileName1, const string& fileName2, const string& f
 
 		n = 1 - n;
 	}
+
+	f1.close(); f2.close(); f3.close();
+}
+
+void merge(const string& fileName1, const string& fileName2, const string& fileName3, const string& fileName4, int p)
+{
+	int x, y;
+	int i, j;
+
+	ofstream f1(fileName1);
+	ofstream f2(fileName2);
+	ifstream f3(fileName3);
+	ifstream f4(fileName4);
+
+	f3 >> x;
+	f4 >> y;
+
+	int n = 0, k = 0;
+	while (!f3.eof() && !f4.eof())
+	{
+		i = j = 0;
+		while (!f3.eof() && !f4.eof() && i < p && j < p)
+		{
+			if (x < y)
+			{
+				if (n == 0)
+				{
+					f1 << x << ' ';
+				}
+				else
+					f2 << x << ' ';
+
+				f3 >> x;
+				i++;
+			}
+
+			else
+			{
+				if (n == 0)
+				{
+					f1 << y << ' ';
+				}
+				else
+					f2 << y << ' ';
+
+				f4 >> y;
+				j++;
+			}
+		}
+
+		while (!f3.eof() && i < p)
+		{
+			if (n == 0)
+			{
+				f1 << x << ' ';
+			}
+			else
+				f2 << x << ' ';
+
+			f3 >> x;
+			i++;
+		}
+
+		while (!f4.eof() && j < p)
+		{
+			if (n == 0)
+			{
+				f1 << y << ' ';
+			}
+			else
+				f2 << y << ' ';
+
+			f4 >> y;
+			j++;
+		}
+
+		n = 1 - n;
+	}
+
+	while (!f3.eof())
+	{
+		if (n == 0)
+		{
+			f1 << x << ' ';
+		}
+		else
+			f2 << x << ' ';
+
+		f3 >> x;
+	}
+
+	while (!f4.eof())
+	{
+		if (n == 0)
+		{
+			f1 << y << ' ';
+		}
+		else
+			f2 << y << ' ';
+
+		f4 >> y;
+	}
+
+	f1.close();
+	f2.close();
+	f3.close();
+	f4.close();
+
 }
 
 int main()
 {		
-	int numbersCount, maxNumberValue;
-	cout << "enter count of numbers:	";
-	cin >> numbersCount;
-	cout << "\nenter max number value:	";
-	cin >> maxNumberValue;
-
-
-	createFileWithRandomNumbers("file.txt", numbersCount, maxNumberValue);
-	
-	splitting("file.txt", "file1.txt", "file2.txt", 2);
-
 	
 
 }
